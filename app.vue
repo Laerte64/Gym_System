2bc704f88f483793c2ae3bfdba769ebd487fd4eb
Cayo-Cezar <cayooliveira@alunos.utfpr.edu.br>
2024-08-13 11:10:32 -0300

    enviando

---
+++ app.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="flex justify-content-center align-items-center h-screen ">
+    <!-- <NuxtRouteAnnouncer />
+    <NuxtWelcome /> -->
+
+    <Button label="Submit" />
+    <Button label="Submit" class="teste" />
+    <Button icon="pi pi-check" aria-label="Submit" />
+  </div>
+</template>
+<style>
+.teste.p-button {
+  background: magenta;
+  border-radius: 0;
+}
+</style>
