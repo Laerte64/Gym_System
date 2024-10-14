83008e3b241037cf784c3cbe418dd11c7c067dc8
Cayo-Cezar <cayooliveira@alunos.utfpr.edu.br>
2024-10-14 13:37:49 -0300

    criacao dos controllers

---
+++ Backend/Controller/MemberController.cs
@@ -0,0 +1,58 @@
+﻿using Data;
+using Model;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace Controller;
+
+[Route("api/member")]
+[ApiController]
+internal class UserController : ControllerBase
+{
+    private readonly GymContext _context;
+    public UserController(GymContext context)
+    {
+        _context = context;
+    }
+
+    [HttpGet]
+    public async Task<IActionResult> GetAll()
+    {
+        var users = await _context.Users.ToListAsync();
+        return Ok(users);
+    }
+
+    [HttpGet("getById")]
+    public async Task<IActionResult> GetById(int id)
+    {
+        var user = await _context.Users.FindAsync(id);
+        return Ok(user);
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> Create(User user)
+    {
+        _context.Add(user);
+        await _context.SaveChangesAsync();
+        return Created("", user);
+    }
+
+    [HttpPut]
+    public async Task<IActionResult> Update(User user)
+    {
+        _context.Entry(user).State = EntityState.Modified;
+        await _context.SaveChangesAsync();
+        return Ok(user);
+    }
+
+    [HttpDelete]
+    public async Task<IActionResult> DeleteById(int id)
+    {
+        var user = await _context.Users.FindAsync(id);
+        if (user == null)
+            return NoContent();
+        _context.Remove(user);
+        await _context.SaveChangesAsync();
+        return Ok();
+    }
+}
